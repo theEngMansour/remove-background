74344733b19fcc70733387632adcd4e161a8cbb4
Mansour Ahmed <mansour.yemen1@gmail.com>
2024-10-15 23:35:45 +0300

    change main

--- remove.py
+++ remove.py
@@ -2,8 +2,8 @@ from rembg import remove
 from PIL import Image
 
 """ input_path """
-input_path = 'cd.jpg'
 output_path = 'output.png'
+input_path = 'cd.jpg'
 
 input = Image.open(input_path)
 output = remove(input)
