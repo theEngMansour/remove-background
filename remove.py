6d911f7a2b7a17d82b45fdfffae5422671857eed
Mansour Ahmed <mansour.yemen1@gmail.com>
2024-10-15 23:53:34 +0300

    deleted

--- remove.py
+++ remove.py
@@ -5,7 +5,3 @@ from PIL import Image
 input_path = 'cd.jpg'
 output_path = 'output.png'
 
-output = remove(input)
-input = Image.open(input_path)
- # the coment inserting by open-img
-output.save(output_path)
