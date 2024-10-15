df27f0f5df88b01ea9d1cbfce939bea4fd6a91a9
Mansour Ahmed <mansour.yemen1@gmail.com>
2024-10-15 15:04:26 +0300

    add

---
+++ remove.py
@@ -0,0 +1,9 @@
+from rembg import remove
+from PIL import Image
+
+input_path = 'cd.jpg'
+output_path = 'output.png'
+
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
